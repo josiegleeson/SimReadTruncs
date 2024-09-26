9e1785b0b166da1b984d121f8b2d7d9b91cc5cc2
Josie Gleeson <30969357+josiegleeson@users.noreply.github.com>
2024-09-26 15:06:24 +1000

    Update SimReadTruncs.R

--- SimReadTruncs.R
+++ SimReadTruncs.R
@@ -5,6 +5,7 @@ suppressWarnings({
     library(stats)
     library(optparse)
     library(dplyr)
+    library(this.path)
   })
 })
   
@@ -13,10 +14,10 @@ option_list = list(
               help="Transcriptome reference FASTA", metavar="character"),
   make_option(c("-c", "--counts_file"), type="character", default=NULL,
               help="Read count file (txt, tsv or csv). Header must be: transcript_id, read_count", metavar="character"),
-  make_option(c("-l", "--read_lengths"), type="character", default=NULL,
+  make_option(c("-l", "--read_lengths"), type="character", default="human",
               help="Read lengths option (sirv, human) or file (txt, tsv or csv). Header must be: lengths. Leave blank for human.", metavar="character"),
-  make_option(c("-o", "--output_file"), type="character", default=NULL,
-              help="Directory to be created and where output FASTA will be written", metavar="character")
+  make_option(c("-o", "--output_file"), type="character", default="output.fasta",
+              help="Output FASTA file (output.fasta)", metavar="character")
 )
 
 opt_parser <- OptionParser(option_list=option_list)
@@ -25,7 +26,7 @@ opt <- parse_args(opt_parser)
 # read in from command line
 txs <- opt$fasta
 count_input <- opt$counts_file
-reads_input <- opt$read_lengths
+read_input <- opt$read_lengths
 output <- opt$output_file
 
 message("Importing data...")
@@ -59,66 +60,97 @@ unique_ids <- unlist(lapply(1:length(original_names), function(i) {
 # set the unique ids as names
 names(counts_txs) <- unique_ids
 
-# read length kde generation
 # kde <- density(bamslam$read_length, from=200, to=20000, na.rm = TRUE)
 # saveRDS(kde, file="~/Documents/drna_sim/kde_sirv.rds")
 
 # set script dir
-dirpath <- dirname(rstudioapi::getSourceEditorContext()$path)
+dirpath <- dirname(this.path())
+kde_3p <- readRDS(file = paste0(dirpath, "/kde_data/kde_3_prime_end.rds"))
 
 # import kde
-if (is.null(read_lengths) | read_lengths == "human") {
-  kde <- readRDS(file = paste0(dirpath, "read_length_kdes/kde_human.rds"))
-} else if (read_lengths == "sirv") {
-  kde <- readRDS(file = paste0(dirpath, "/read_length_kdes/kde_sirv.rds"))
+if (is.null(read_input) | read_input == "human") {
+  kde_read_length <- readRDS(file = paste0(dirpath, "/kde_data/kde_read_length_human_brain.rds"))
+  kde_long_read_length <- readRDS(file = paste0(dirpath, "/kde_data/kde_long_read_length_human_brain.rds"))
+  message("Using provided human read length model")
+} else if (read_input == "sirv") {
+  kde_read_length <- readRDS(file = paste0(dirpath, "/kde_data/kde_read_length_sirv.rds"))
+  kde_long_read_length <- readRDS(file = paste0(dirpath, "/kde_data/kde_long_read_length_sirv.rds"))
+  message("Using provided sirv read length model")
 } else {
-  length_data <- fread(read_lengths)
-  length_data$nt_col <- length_data[,1] 
+  custom_data <- fread(read_input)
+  length_data$nt_col <- length_data[,1]
   length_data$nt_col <- as.numeric(length_data$nt_col)
-  kde <- density(length_data$nt_col, from=200, to=50000, na.rm = TRUE)
+  
+  kde_read_length <- density(length_data$nt_col, from=200, to=20000, na.rm = TRUE)
 }
 
-# sample from kde
+# function to sample from kde
 sample_from_kde <- function(kde) {
   
   # get length
-  sample_read_length <- sample(kde$x, size = 1, prob = kde$y)
-  
-  return(sample_read_length)
+  sample_length <- sample(kde$x, size = 1, prob = kde$y)
+  return(sample_length)
   
 }
 
-# remove nt from the 5' end of sequence
-remove_kde_length <- function(seq) {
+# function to remove nt from the 5' end of sequence
+remove_kde_length <- function(seq, max_attempts = 10) {
   
   # get tx length
   seq_length <- nchar(seq)
   
-  # get read length from kde
-  simulated_read_length <- floor(sample_from_kde(kde))
-  
-  random_3p_end <- sample(1:10, 1)
-  
-  if (simulated_read_length >= seq_length & seq_length > 50 & seq_length < 1000) {
-   
-    # if nt to remove is more than sequence length (and length is <1k), return 20 nt truncation
-    return(subseq(seq, start = 20, end = -abs(random_3p_end)))
-    
-  } else if (simulated_read_length >= seq_length & seq_length > 1000) {
-    
-    # if nt to remove is more than sequence length (and length is >1k), return 200 nt truncation
-    return(subseq(seq, start = 200, end = -abs(random_3p_end)))
-    
-  } else if (seq_length < 50) {
+  # to add: 5% of the time, no truncation occurs
+  random_number <- sample(1:20, 1)
+
+  if (random_number == 10 & seq_length < 20000) {
     
     # no truncation
     return(seq)
     
-  } else { 
+  } else {
     
-    # remove kde based number of nt
-    return(subseq(seq, start = -abs(simulated_read_length), end = -abs(random_3p_end)))
+    # determine which kde to use
+    if (seq_length > 3000) {
+      kde_to_sample_from <- kde_long_read_length
+    } else {
+      kde_to_sample_from <- kde_read_length
+    }
     
+    # sample from kde until read length is less than tx length
+    attempts <- 0
+    repeat {
+      # sample from read length kde defined above
+      simulated_read_length <- floor(sample_from_kde(kde_to_sample_from))
+      attempts <- attempts + 1
+      if (simulated_read_length < seq_length | attempts >= max_attempts) break
+    }
+    
+    # if no valid length after max_attempts, use seq_length - 10
+    if (attempts >= max_attempts) {
+      simulated_read_length <- seq_length - 10
+    }
+    
+    # ensure simulated_read_length is at least 1
+    simulated_read_length <- max(1, simulated_read_length)
+    
+    # generate 3' end truncation
+    simulated_3p_truncation <- floor(sample_from_kde(kde_3p))
+    
+    # ensure simulated_3p_truncation is not larger than the sequence length
+    simulated_3p_truncation <- min(simulated_3p_truncation, seq_length - 1)
+    
+    # get read start and end positions
+    start <- max(1, seq_length - simulated_read_length + 1)
+    end <- max(start, seq_length - simulated_3p_truncation)  # ensure end is not less than start
+    
+    # final check to ensure start <= end <= seq_length
+    if (start > end | end > seq_length) {
+      # if coords are still invalid return the full sequence
+      return(seq)
+    }
+    
+    # return truncated read
+    return(subseq(seq, start = start, end = end))
   }
   
 }
@@ -133,3 +165,4 @@ writeXStringSet(trunc_txs, paste0(output), append=FALSE,
                 compress=FALSE, compression_level=NA, format="fasta")
 
 message(paste0("Created ", output))
+
